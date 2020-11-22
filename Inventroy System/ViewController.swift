9024e7b76c392c54bdaeadd0efd5d831903ba3c1
ahmednasr8898 <ans96283@gmail.com>
2020-11-23 01:01:30 +0200

    Initial Commit

---
+++ Inventroy System/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Inventroy System
+//
+//  Created by Ahmed Nasr on 11/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
